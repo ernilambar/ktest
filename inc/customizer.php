edd9343cc611c7c32c57faade52d93b528c613cc
Nilambar Sharma <nilambar@outlook.com>
2020-02-21 22:39:32 +0545

    Add fiel

--- inc/customizer.php
+++ inc/customizer.php
@@ -6,50 +6,10 @@
  */
 
 /**
- * Add postMessage support for site title and description for the Theme Customizer.
+ * Customize options.
  *
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
-function ktest_customize_register( $wp_customize ) {
-	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
-	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
-
-	if ( isset( $wp_customize->selective_refresh ) ) {
-		$wp_customize->selective_refresh->add_partial( 'blogname', array(
-			'selector'        => '.site-title a',
-			'render_callback' => 'ktest_customize_partial_blogname',
-		) );
-		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
-			'selector'        => '.site-description',
-			'render_callback' => 'ktest_customize_partial_blogdescription',
-		) );
-	}
-}
-add_action( 'customize_register', 'ktest_customize_register' );
-
-/**
- * Render the site title for the selective refresh partial.
- *
- * @return void
- */
-function ktest_customize_partial_blogname() {
-	bloginfo( 'name' );
-}
-
-/**
- * Render the site tagline for the selective refresh partial.
- *
- * @return void
- */
-function ktest_customize_partial_blogdescription() {
-	bloginfo( 'description' );
-}
-
-/**
- * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
- */
-function ktest_customize_preview_js() {
-	wp_enqueue_script( 'ktest-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
-}
-add_action( 'customize_preview_init', 'ktest_customize_preview_js' );
+add_action( 'customize_register', function( $wp_customize ) {
+$wp_customize->register_control_type( '\Kirki\Field\Typography' );
+});
